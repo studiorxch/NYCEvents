2842c4b2a76bf4974bede2769d0f9365614622c4
Richie Lau <whatsup@richielau.com>
2025-04-24 00:38:09 -0400

    Sound Vibe check

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.post('/api/add-event', async (req, res) => {
                     "Borough": { select: { name: borough } },
                     "Location": { rich_text: [{ text: { content: location } }] },
                     "Google Maps": { url: mapsLink },
-                    "Sound Vibe": { multi_select: soundVibe.split(',').map(item => ({ name: item.trim() })) },
+                    "Sound Vibe": { multi_select: soundVibe.split(',').map(v => ({ name: v.trim() })) },
                     "Perks": { rich_text: [{ text: { content: perks } }] },
                     "Weather Suitable": { select: { name: weatherSuitable } },
                     "Expected Crowd Level": { select: { name: crowdLevel } },
